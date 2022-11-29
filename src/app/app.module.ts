3798c7683ae4dba48f00657f5d2cfbf726d75827
Alvaro <r.otarola13@gmail.com>
2022-11-28 22:55:17 -0300

    add login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
+// import { RegistroComponent } from './components/registro/registro.component';
+// import { DestinatarioComponent } from './components/destinatario/destinatario.component';
+// import { HistoricoComponent } from './components/historico/historico.component';
+// import { TransferenciaComponent } from './components/transferencia/transferencia.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    // DestinatarioComponent,
+    // RegistroComponent,
+    // HistoricoComponent,
+    // TransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
