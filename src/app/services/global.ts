1ffca7738377fc04a253c547543e2e62eefa11e6
Alvaro <r.otarola13@gmail.com>
2022-12-03 22:52:04 -0300

    fix url

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,6 +1,6 @@
 export var GLOBAL = {
-    url:'http://localhost:5000/api/',
-     // url:'https://desafioalvaroriquelmeback-production.up.railway.app/api/',
+    //url:'http://localhost:5000/api/',
+    url:'https://desafioalvaroriquelmeback-production.up.railway.app/api/',
     urlBanks:"https://bast.dev/api/banks.php",
     ip:'127.0.0.1',
 
